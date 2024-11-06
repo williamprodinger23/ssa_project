d9c0946288da61b28883c0090b10d28353c37267
williamprodinger <william.prodinger@student.uts.edu.au>
2024-11-06 14:47:05 +1100

    added group creation

--- ssa_project/settings.py
+++ ssa_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-@3repq61gboroo!mu-#qn64iy$rimbq7!h_w1=@qjdgw_yr@&=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+RECAPTCHA_SECRET_KEY = "6LeMRm4qAAAAAPslEmmSL7zQBpwLV-YHw0R99ytB"
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.user, name="user"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path('register/', views.register, name='register'),
 ]
