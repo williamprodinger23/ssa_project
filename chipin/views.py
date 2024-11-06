324dc51956ab5810686c08c1afe57766a60a9f6e
williamprodinger <william.prodinger@student.uts.edu.au>
2024-11-06 14:55:00 +1100

    Nicknames replace Email

--- chipin/views.py
+++ chipin/views.py
@@ -50,10 +50,10 @@ def invite_users(request, group_id):
         user_id = request.POST.get('user_id')
         invited_user = get_object_or_404(User, id=user_id)      
         if invited_user in group.invited_users.all():
-            messages.info(request, f'{invited_user.username} has already been invited.')
+            messages.info(request, f'{invited_user.profile.nickname} has already been invited.')
         else:
             group.invited_users.add(invited_user)
-            messages.success(request, f'Invitation sent to {invited_user.username}.')
+            messages.success(request, f'Invitation sent to {invited_user.profile.nickname}.')
         return redirect('chipin:group_detail', group_id=group.id)  
     return render(request, 'chipin/invite_users.html', {
         'group': group,
@@ -67,11 +67,11 @@ def accept_invite(request, group_id):
     if user_id:
         invited_user = get_object_or_404(User, id=user_id)
         if invited_user in group.members.all():
-            messages.info(request, f'{invited_user.username} is already a member of the group "{group.name}".')
+            messages.info(request, f'{invited_user.profile.nickname} is already a member of the group "{group.name}".')
         elif invited_user in group.invited_users.all():
             group.members.add(invited_user)
             group.invited_users.remove(invited_user)  # Remove from invited list
-            messages.success(request, f'{invited_user.username} has successfully joined the group "{group.name}".')
+            messages.success(request, f'{invited_user.profile.nickname} has successfully joined the group "{group.name}".')
         else:
             messages.error(request, "You are not invited to join this group.")
     else:
